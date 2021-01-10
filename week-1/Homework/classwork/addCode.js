7890a450cd92ea5fa14c5227cc957e8216110034
jwillson2020 <73646450+jwillson2020@users.noreply.github.com>
2021-01-10 13:48:44 +0000

    added addCode
    seem to work

---
+++ week-1/Homework/classwork/addCode.js
@@ -0,0 +1,13 @@
+/*
+Write code in the space provided so that it outputs "Gilbert"
+*/
+
+// WRITE CODE BELOW THIS
+let kitten = {
+name: 'Gilbert', // output Gilbert
+age: 2,
+};
+// WRITE CODE ABOVE THIS
+
+console.log(kitten.name);
+// -> it should output: "Gilbert"
